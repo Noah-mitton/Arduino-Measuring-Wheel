a14723d0460b31acc9fd868c15294d4695824bdd
Noah-mitton <122578674+Noah-mitton@users.noreply.github.com>
2023-01-12 16:58:01 -0800

    Add files via upload

---
+++ Measuring_Wheel.ino
@@ -0,0 +1,234 @@
+int segA = 2;
+int segB = 3;
+int segC = 4;
+int segD = 5;
+int segE = 6;
+int segF = 7;
+int segG = 8;
+int digit1 = 9;
+int digit2 = 10;
+int digit3 = 11;
+int digit4 = 12;
+
+#define inputCLK A1
+#define inputDT A2
+ 
+
+
+int counter = 0; 
+int currentStateCLK;
+int previousStateCLK; 
+
+String value ="";
+
+void setup() {                
+  // initialize the digital pins as outputs.
+  pinMode(segA, OUTPUT);     
+  pinMode(segB, OUTPUT);     
+  pinMode(segC, OUTPUT);     
+  pinMode(segD, OUTPUT);     
+  pinMode(segE, OUTPUT);     
+  pinMode(segF, OUTPUT);     
+  pinMode(segG, OUTPUT);   
+  pinMode(digit1, OUTPUT);  
+  pinMode(digit2, OUTPUT);  
+  pinMode(digit3, OUTPUT);  
+  pinMode(digit4, OUTPUT);
+ 
+  pinMode (inputCLK,INPUT);
+  pinMode (inputDT,INPUT);
+
+  Serial.begin (9600);
+   
+  previousStateCLK = digitalRead(inputCLK);
+
+}
+
+void loop() {
+  
+  currentStateCLK = digitalRead(inputCLK);
+    
+   
+   if (currentStateCLK != previousStateCLK){ 
+       
+     if (digitalRead(inputDT) != currentStateCLK) { 
+       counter --;
+       value ="CCW";
+       
+     } else {
+       counter ++;
+       value ="CW";
+       
+     }
+     Serial.print("Direction: ");
+     Serial.print(value);
+     Serial.print(" -- Value: ");
+     Serial.println(counter);
+   } 
+   previousStateCLK = currentStateCLK;
+
+   displayNumber01(counter/8.09);
+}
+
+void displayNumber01(int toDisplay) {
+#define DISPLAY_BRIGHTNESS  500
+
+#define DIGIT_ON  HIGH
+#define DIGIT_OFF  LOW
+
+  for(int digit = 4 ; digit > 0 ; digit--) {
+
+    //Turn on a digit for a short amount of time
+    switch(digit) {
+    case 1:
+     digitalWrite(digit1, DIGIT_ON);
+     //digitalWrite(segDP, HIGH);
+      break;
+   case 2:
+      digitalWrite(digit2, DIGIT_ON);
+      //digitalWrite(segDP, LOW);
+      break;
+    case 3:
+      digitalWrite(digit3, DIGIT_ON);
+      //digitalWrite(segDP, HIGH);
+      break;
+    case 4:
+      digitalWrite(digit4, DIGIT_ON);
+      //digitalWrite(segDP, HIGH);
+      break;
+    }
+    lightNumber(toDisplay % 10);
+    toDisplay /= 10;
+    delayMicroseconds(DISPLAY_BRIGHTNESS); 
+
+     //Turn off all segments
+    lightNumber(10); 
+
+    //Turn off all digits
+    digitalWrite(digit1, DIGIT_OFF);
+    digitalWrite(digit2, DIGIT_OFF);
+    digitalWrite(digit3, DIGIT_OFF);
+    digitalWrite(digit4, DIGIT_OFF);
+}
+} 
+
+void lightNumber(int numberToDisplay) {
+
+#define SEGMENT_ON  LOW
+#define SEGMENT_OFF HIGH
+
+  switch (numberToDisplay){
+
+  case 0:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 1:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 2:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 3:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 4:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 5:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 6:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 7:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+
+  case 8:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_ON);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  case 9:
+    digitalWrite(segA, SEGMENT_ON);
+    digitalWrite(segB, SEGMENT_ON);
+    digitalWrite(segC, SEGMENT_ON);
+    digitalWrite(segD, SEGMENT_ON);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_ON);
+    digitalWrite(segG, SEGMENT_ON);
+    break;
+
+  // all segment are ON
+  case 10:
+    digitalWrite(segA, SEGMENT_OFF);
+    digitalWrite(segB, SEGMENT_OFF);
+    digitalWrite(segC, SEGMENT_OFF);
+    digitalWrite(segD, SEGMENT_OFF);
+    digitalWrite(segE, SEGMENT_OFF);
+    digitalWrite(segF, SEGMENT_OFF);
+    digitalWrite(segG, SEGMENT_OFF);
+    break;
+  
+  }
+}
